d5d562722321f642c96fb15c2f4b5ca6853f00e5
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-21 20:30:55 +1300

    Get computer and human choice
    Added functions to get the computer and human choices
    Human choice is assigned at random if no valid entry is made

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,56 @@
 console.log("Hi, there!");
+
+function getComputerChoice() {
+    let computerChoice;
+    const options = 3;
+    
+    // Make a random choice out of 3 for the computer to play.
+    switch (Math.floor(Math.random() * options)) {
+        case 0:
+            computerChoice = "Rock"
+            break;
+        case 1:
+            computerChoice = "Paper"
+            break;
+        default:
+            computerChoice = "Scissors"
+            break;
+    }
+    return computerChoice
+}
+
+function getHumanChoice() {
+    
+    let humanChoice = prompt("What do you pick? Rock, paper or scissors");
+    console.log(humanChoice.slice(0, 1))
+    console.log(("rock" || "paper" || "scissors"));
+    
+
+    switch (humanChoice.slice(0, 1)) {
+        case ("r"):
+            humanChoice = "Rock"
+            alert("You chose: " + humanChoice);
+            break;
+        case ("p"):
+            humanChoice = "Paper"
+            alert("You chose: " + humanChoice);
+            break;
+         case ("s"):
+            humanChoice = "Scissors"
+            alert("You chose: " + humanChoice);
+            break;
+        default:
+            humanChoice = getComputerChoice();
+            alert("It was chosen for you: " + humanChoice);
+            break;
+    }
+
+    // Replace if statement below with switch case above
+    // Problem with || operator and printing past first check option to console
+    // if (humanChoice.toLowerCase() === ("rock" || "paper" || "scissors")) {
+    //     console.log(humanChoice);
+    //     // alert("You chose: " + message);
+    // } 
+    
+    return humanChoice;
+}
