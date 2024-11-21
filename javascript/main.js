b5f2be7fa15b0905b5ff3264e0376d653361e1a3
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-21 22:03:48 +1300

    Add round logic
    Added playRound function
    Decides who is the winner of the round

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,11 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let computerChoice;
+    let computerChoice = Math.floor(Math.random() * options);
     const options = 3;
     
     // Make a random choice out of 3 for the computer to play.
-    switch (Math.floor(Math.random() * options)) {
+    switch (computerChoice) {
         case 0:
             computerChoice = "Rock"
             break;
@@ -23,7 +23,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     
-    let humanChoice = prompt("What do you pick? Rock, paper or scissors");
+    let humanChoice = prompt("What do you pick? Rock, paper or scissors").toLowerCase();
     
     switch (humanChoice.slice(0, 1)) {
         case ("r"):
@@ -43,6 +43,24 @@ function getHumanChoice() {
             alert("It was chosen for you: " + humanChoice);
             break;
     }
-    
     return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock") {
+        computerChoice === "Rock" ? console.log("Tied round") :
+        computerChoice === "Paper" ? (console.log("“You lose! Paper beats Rock”."), computerScore++)  :
+        computerChoice === "Scissors" ? (console.log("“You win! Rock beats Scissors”."), humanScore++) : "";    
+    } else if (humanChoice === "Paper") {
+        computerChoice === "Paper" ? console.log("Tied round") :
+        computerChoice === "Rock" ? (console.log("“You win! Paper beats Rock”."), humanScore++) :
+        computerChoice === "Scissors" ? (console.log("“You lose! Scissors beats Paper”."), computerScore++) : ""
+    } else {
+        computerChoice === "Scissors" ? console.log("Tied round") :
+        computerChoice === "Paper" ? (console.log("“You win! Scissors beats Paper”."), humanScore++) :
+        computerChoice === "Rock" ? (console.log("“You lose! Rock beats Scissors”."), computerScore++) : ""
+    }
+    
+    console.log(humanChoice + "\t" + humanScore);
+    console.log(computerChoice + "\t" + computerScore);
 }
