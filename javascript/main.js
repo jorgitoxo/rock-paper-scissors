7c14f37ff7cb491e5092125759cf052dfee38b6a
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-07 02:42:47 +1300

    Play until someone reaches 5 points
    Removed individual button event listeners
    Used event delegation to handle click on buttons
    Changed console logs to DOM events to display messages in page body
    Game resets once someone reaches 5 points

--- javascript/main.js
+++ javascript/main.js
@@ -1,63 +1,78 @@
+const maxScore = 5;
 let humanScore = 0;
 let computerScore = 0;
-const maxScore = 5;
-let humanChoice = '';
-let computerChoice = '';
-const rockButton = document.querySelector("#rock");
-const paperButton = document.querySelector("#paper");
-const scissorsButton = document.querySelector("#scissors");
-
+const buttonsPanel = document.querySelector(".buttons-panel");
+const scoreboardPanel = document.querySelector(".scoreboard-panel");
+const announcementPanel = document.querySelector(".announcement-panel");
 
 // Make a random choice out of 3 options for the computer to play.
 function getComputerChoice() {
     const options = 3;
     let computerChoice = Math.floor(Math.random() * options);
-    return computerChoice = computerChoice === 0 ? 
-        "Rock" : computerChoice === 1 ? 
-        "Paper" : "Scissors";
+    return computerChoice = computerChoice === 0 ? "Rock" 
+        : computerChoice === 1 ? "Paper" 
+        : "Scissors";
 }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "Rock") {
-        computerChoice === "Rock" ? console.log("Tied round...") :
-        computerChoice === "Paper" ? (console.log("“You lose! Paper beats Rock”."), computerScore++)  :
-        computerChoice === "Scissors" ? (console.log("“You win! Rock beats Scissors”."), humanScore++) : "";    
+        computerChoice === "Rock" ?
+            (announcementPanel.textContent = "Tied round...")
+        : computerChoice === "Paper" ?
+            ((announcementPanel.textContent = `“You lose! Paper beats Rock.”`), computerScore++)
+        : computerChoice === "Scissors" ?
+            ((announcementPanel.textContent = `“You win! Rock beats Scissors.”`), humanScore++)
+        : "";
     } else if (humanChoice === "Paper") {
-        computerChoice === "Paper" ? console.log("Tied round...") :
-        computerChoice === "Rock" ? (console.log("“You win! Paper beats Rock”."), humanScore++) :
-        computerChoice === "Scissors" ? (console.log("“You lose! Scissors beats Paper”."), computerScore++) : ""
+        computerChoice === "Paper" ?
+            (announcementPanel.textContent = `"Tied round..."`)
+        : computerChoice === "Rock" ?
+            ((announcementPanel.textContent = `“You win! Paper beats Rock.”`), humanScore++)
+        : computerChoice === "Scissors" ?
+            ((announcementPanel.textContent = `“You lose! Scissors beats Paper.”`), computerScore++)
+        : "";
     } else {
-        computerChoice === "Scissors" ? console.log("Tied round...") :
-        computerChoice === "Paper" ? (console.log("“You win! Scissors beats Paper”."), humanScore++) :
-        computerChoice === "Rock" ? (console.log("“You lose! Rock beats Scissors”."), computerScore++) : ""
+        computerChoice === "Scissors" ?
+            (announcementPanel.textContent = `"Tied round..."`)
+        : computerChoice === "Paper" ?
+            ((announcementPanel.textContent = `“You win! Scissors beats Paper.”`), humanScore++)
+        : computerChoice === "Rock" ? 
+            ((announcementPanel.textContent = `“You lose! Rock beats Scissors.”`), computerScore++)
+        : "";
     }
-    console.log(humanChoice + "\t" + computerChoice + "\n" + "H: " + humanScore + "\t" + "C: " + computerScore);
+    
+    scoreboardPanel.textContent = `Player:[${humanScore}]` + "\n" + `Computer:[${computerScore}]`;
+    if ((humanScore === maxScore) || (computerScore === maxScore)) {
+        announceResults(humanScore, computerScore);
+        reset();
+    }
 }
 
 function announceResults(humanScore, computerScore) {
-    humanScore === computerScore ? (console.log("*** Tie! ***"), alert("*** Tie! ***")) :
-    humanScore > computerScore ? (console.log("*** You are winner!!! ***"), alert("*** You are winner!!! ***")) : 
-    (console.log("*** You are loser... ***"), alert("*** You are loser... ***"));
+    humanScore > computerScore ? ((announcementPanel.textContent = `*** You are winner!!! ***`), alert("*** You are winner!!! ***"))
+    : ((announcementPanel.textContent = `*** You are loser... ***`), alert("*** You are loser... ***"));
+    return;
+}
+
+function reset () {
+    humanScore = 0;
+    computerScore = 0;
+    announcementPanel.textContent = 'Play again?';
 }
 
 function playGame() {
-    rockButton.addEventListener('click', (humanChoice, computerChoice) => {
-        computerChoice = getComputerChoice();
-        humanChoice = rockButton.textContent;
-        playRound(humanChoice, computerChoice);
-    });
+    let computerChoice = getComputerChoice();
+    buttonsPanel.addEventListener('click', (event) => {
+        let humanChoice = event.target.textContent;
+        playRound(humanChoice, computerChoice);    
+    });    
 
-    paperButton.addEventListener('click', (humanChoice, computerChoice) => {
-        computerChoice = getComputerChoice();
-        humanChoice = paperButton.textContent;
-        playRound(humanChoice, computerChoice);
-    });
-
-    scissorsButton.addEventListener('click', (humanChoice, computerChoice) => {
-        computerChoice = getComputerChoice();
-        humanChoice = scissorsButton.textContent;
-        playRound(humanChoice, computerChoice);
-    });
+    // why does this result in a page crash?
+    // while ((humanScore && computerScore) < maxScore) {
+    //     playRound(humanChoice, computerChoice); 
+    // }
+    // announceResults();
 }
 
 playGame();
+
